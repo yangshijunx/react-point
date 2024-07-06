4b053012477010209756563c92d8567b69c6771e
Apple <yangshijunxxx@gmail.com>
2024-07-06 23:29:55 +0800

    feat: useEffect

--- src/pages/useEffect/index.tsx
+++ src/pages/useEffect/index.tsx
@@ -1,4 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useInsertionEffect,
+} from "react";
 
 const UseEffect: React.FC = () => {
   const [count, setCount] = useState<number>(0);
@@ -37,6 +42,29 @@ const UseEffect: React.FC = () => {
       console.log(`Count before update: ${count}`);
     };
   }, [count]); // 依赖 count，count 变化时执行
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect called");
+  }, [count]);
+
+  //   在所有 DOM 变更之前运行，所以它只会在组件挂载时运行一次。
+  useInsertionEffect(() => {
+    console.log("useInsertionEffect called");
+    // useInsertionEffect 只会执行一次 uesEffect和uselayoutEffect执行了两次
+    // 在严格模式下（React.StrictMode），React 会在开发环境中有意双调用（mount 和 unmount）组件的生命周期方法和副作用，
+    // 以帮助开发者发现意外的副作用。这是为了确保副作用逻辑是幂等的（即多次调用不会产生副作用）。
+    // 添加样式
+    const style = document.createElement("style");
+    style.innerHTML = `
+      body {
+        background-color: ${count % 2 === 0 ? "red" : "blue"};
+    }`;
+    document.head.appendChild(style);
+    return () => {
+      console.log("useInsertionEffect cleanup called");
+      document.head.removeChild(style);
+    };
+  }, [count]);
 
   return (
     <div>
